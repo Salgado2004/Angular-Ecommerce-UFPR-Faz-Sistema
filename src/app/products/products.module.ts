7934226d08ffdc37576887840ddc49a0b79c91b8
Salgado2004 <leosalgado2004@gmail.com>
2023-09-18 13:55:28 -0300

    import and declare new components

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { ProductsRoutingModule } from './products-routing.module';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { RatingComponent } from './rating/rating.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListProductsComponent,
+    ProductCardComponent,
+    RatingComponent
+  ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ProductsRoutingModule
   ]
 })
 export class ProductsModule { }
