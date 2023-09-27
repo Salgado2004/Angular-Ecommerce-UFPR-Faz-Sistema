3d4cac6211c42272f5da2699db171dcba4ed4ec5
Salgado2004 <leosalgado2004@gmail.com>
2023-09-27 20:04:33 -0300

    import httpClient to access mock data

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ListProductsComponent } from './list-products/list-products.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { RatingComponent } from './rating/rating.component';
+import { ProductsService } from './products.service';
 
 
 
@@ -19,7 +21,9 @@ import { RatingComponent } from './rating/rating.component';
     CommonModule,
     SharedModule,
     ProductsRoutingModule,
-    FormsModule
-  ]
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [ ProductsService]
 })
 export class ProductsModule { }
